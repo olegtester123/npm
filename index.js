b3f10d9017b05e328c647bf297561e4d012a0eaf
Oleg Shprengel <olegshprengel@Olegs-MacBook-Pro.local>
2026-03-13 10:02:05 -0600

    changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const chalk = require('chalk');
 const mpath = require('mpath');
 
+// SAST: eval with user-controlled input (e.g. from CLI or env)
+const userExpr = process.env.USER_EXPR || process.argv[2];
+if (userExpr) eval(userExpr);
+
 console.log(chalk.blue('Hello from your npm package!'));
 console.log('mpath (phantom):', mpath.version || 'loaded');
